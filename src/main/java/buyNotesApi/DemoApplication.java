721ca70d2b95517b04b2c1acaf5c7e46b8ddac16
jorge <jorge.antequera@live.u-tad.com>
2026-03-11 16:13:17 +0100

    Primer commit

---
+++ src/main/java/buyNotesApi/DemoApplication.java
@@ -0,0 +1,37 @@
+package buyNotesApi;
+
+import java.util.HashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+//@CrossOrigin(origins = "*")
+public class DemoApplication {
+	
+	  @GetMapping("/crear")
+	  public ResponseEntity<?> crearPartida(@RequestParam(value = "username") String username) {
+		return null;
+		
+	  
+	  
+	  }
+	
+	
+	//meter jackson y jwt y hibernate
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/buyNotesApi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package buyNotesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
